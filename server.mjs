2d7c56b1ce1506c25d902197c1e885edc035fec4
Reja Fatima <164767074+Rija2005@users.noreply.github.com>
2025-02-14 17:53:31 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 console.log("hello world");
 import express from "express"
 const app = express()
-const port = 5000
+const port =process.env.port||5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
