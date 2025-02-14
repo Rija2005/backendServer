ab0373a2b430e5b7062b791fe4d5c451420a32fe
Reja Fatima <syedarija2005@gmail.com>
2025-02-14 17:12:38 +0500

    my work

---
+++ server.mjs
@@ -0,0 +1,13 @@
+console.log("hello world");
+import express from "express"
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  console.log(req.ip);
+})
+//192.168.228.15:5000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
